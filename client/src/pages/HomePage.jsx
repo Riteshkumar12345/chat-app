a7f2f0dc1693e4c3a76da755f06626a6aa75aea2
Riteshkumar12345 <ritesh.kumar3_cs22@gla.ac.in>
2025-05-23 19:57:11 +0530

    done

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -11,9 +11,9 @@ const HomePage = () => {
       <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden
       h-[100%] grid grid-cols-1 relative ${selcetedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_2fr]'
     :'md:grid-cols-2'}`}>
-        <Sidebar/>
-        <ChatContainer/>
-        <RightContainer/>
+        <Sidebar selcetedUser={selcetedUser} setSelectedUser={setSelectedUser}/>
+        <ChatContainer selcetedUser={selcetedUser} setSelectedUser={setSelectedUser} />
+        <RightContainer selcetedUser={selcetedUser} setSelectedUser={setSelectedUser}/>
       </div>
     </div>
   )
