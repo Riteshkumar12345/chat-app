6331eebd45e63ce98d9c24592c88a87bbeeb8020
Riteshkumar12345 <ritesh.kumar3_cs22@gla.ac.in>
2025-05-29 14:41:30 +0530

    user sign login is ready

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRoutes from "./routes.js/userRoutes.js";
+
+const app = express();
+const server = http.createServer(app);
+
+
+app.use(express.json({limit: "4mb"}));
+app.use(cors());
+
+app.use("/api/status", (req, res) =>
+    res.send("Server is live")
+ );
+ app.use("/api/auth", userRoutes);
+ 
+
+ await connectDB();
+ const PORT = process.env.PORT || 5000;
+ server.listen(PORT, ()=> console.log("Server is running no PORT:" +PORT) );
